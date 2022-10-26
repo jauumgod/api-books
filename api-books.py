a4238a77bbc875abb649130d976b61852c3b2673
Joao Marcos D M <69704112+jauumgod@users.noreply.github.com>
2022-10-26 10:14:31 -0300

    Update api-books.py

--- api-books.py
+++ api-books.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-app.config['SECRET_KEY]= 'your_secret_key'
+app.config['SECRET_KEY']= 'your_secret_key'
 books_ = [
     {
   'id': 1,
